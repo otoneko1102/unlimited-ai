625928b35a77fc16ee445ce4c0014361f268ab42
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-02-23 02:29:48 +0900

    早期アクセス機能を追加

---
+++ canary/lib/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  API_URL: 'https://capi.voids.top/v1/chat/completions',
+  MODELS_URL: 'https://capi.voids.top/v1/models',
+  AVAILABLE_MODELS_URL: 'https://raw.githubusercontent.com/otoneko1102/unlimited-ai/refs/heads/main/canary/data/models.json'
+}
